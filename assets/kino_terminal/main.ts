1d3ebac2ca9bbdbc1cd1c97d9421f417c508a54c
Michael Ruoss <michael@michaelruoss.ch>
2023-11-29 13:35:27 +0100

    chores: Upgrade xterm.js to 5.3.0

--- assets/kino_terminal/main.ts
+++ assets/kino_terminal/main.ts
@@ -3,10 +3,10 @@ import { KinoTerminalAttrs } from './types'
 
 export async function init(ctx: KinoContext, attrs: KinoTerminalAttrs) {
   await ctx.importCSS(
-    'https://cdn.jsdelivr.net/npm/xterm@5.0.0/css/xterm.min.css',
+    'https://cdn.jsdelivr.net/npm/xterm@5.3.0/css/xterm.min.css',
   )
   await ctx.importJS(
-    'https://cdn.jsdelivr.net/npm/xterm@5.0.0/lib/xterm.min.js',
+    'https://cdn.jsdelivr.net/npm/xterm@5.3.0/lib/xterm.min.js',
   )
 
   ctx.root.innerHTML = `
