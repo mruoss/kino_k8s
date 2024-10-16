dcceee9b2a374cf0a8b8b63bb9371a36ca296619
Michael Ruoss <michael.ruoss@cradle.bio>
2024-10-16 20:42:43 +0200

    use livebook/kino-bundler to bundle JS

--- assets/shared/conn_notice.tsx
+++ assets/shared/conn_notice.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
+
 const ConnNotice = () => (
-  <div className="mb-5 rounded-md bg-gray-100 p-4 font-inter text-sm font-medium text-gray-500">
+  <div className="font-inter mb-5 rounded-md bg-gray-100 p-4 text-sm font-medium text-gray-500">
     <p>
       To make requests to the Kubernetes cluster, you need at least one
       connection to a cluster.
---
+++ assets/shared/select.tsx
@@ -0,0 +1,67 @@
+import { RiArrowDownSLine } from '@remixicon/react'
+import classNames from 'classnames'
+import React from 'react'
+
+type SelectProps = {
+  name: string
+  label: string
+  options: { value: string; label: string }[]
+  selectedOption?: string
+  onChange: (option: string) => void
+  className?: string
+  orientation?: 'horiz' | 'vert'
+}
+
+const Select = ({
+  name,
+  label,
+  options,
+  selectedOption,
+  onChange,
+  className = '',
+  orientation = 'vert',
+}: SelectProps) => (
+  <div
+    className={classNames(
+      {
+        'flex flex-row items-baseline': orientation == 'horiz',
+      },
+      className,
+    )}
+  >
+    <label
+      htmlFor={name}
+      className={classNames(
+        {
+          block: orientation == 'vert',
+          'pr-1 uppercase': orientation == 'horiz',
+        },
+        'mb-1 text-sm font-medium',
+      )}
+    >
+      {label}
+    </label>
+    <div className="relative block">
+      <select
+        id={name}
+        value={selectedOption || undefined}
+        onChange={(e) => onChange(e!.target!.value)}
+        className={classNames(
+          { 'block w-full': orientation == 'vert' },
+          'appearance-none rounded-lg border border-gray-300 bg-gray-50 bg-[length:10px] bg-[center_right_10px] bg-no-repeat p-2 pr-5 text-sm focus:border-blue-500 focus:ring-blue-500',
+        )}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+        <RiArrowDownSLine size={16} />
+      </div>
+    </div>
+  </div>
+)
+
+export default Select
